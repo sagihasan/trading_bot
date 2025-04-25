ae1122f1d9e3b08c9f3b028db5018121067f46fa
sagihasan <sagihasan14@gmail.com>
2025-04-25 14:58:15 +0300

    Update discord_test.py

--- bot/discord_test.py
+++ bot/discord_test.py
@@ -7,13 +7,17 @@ load_dotenv()
 public_webhook = os.getenv("DISCORD_PUBLIC_WEBHOOK")
 private_webhook = os.getenv("DISCORD_PRIVATE_WEBHOOK")
 
+
 def send_discord_message(webhook_url, message):
     data = {'content': message}
     response = requests.post(webhook_url, json=data)
     if response.status_code == 204:
         print(f"ההודעה נשלחה בהצלחה ל־Webhook: {webhook_url}")
     else:
-        print(f"שגיאה בשליחה ל־Webhook: {webhook_url} - קוד {response.status_code}")
+        print(
+            f"שגיאה בשליחה ל־Webhook: {webhook_url} - קוד {response.status_code}"
+        )
+
 
 print("בדיקת Webhook ציבורי...")
 send_discord_message(public_webhook, "זהו רק טסט ציבורי.")
