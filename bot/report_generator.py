901dec24e046bbec0772b0a509a3ab8e09d698c0
sagihasan <sagihasan14@gmail.com>
2025-04-27 15:32:17 +0300

    Update report_generator.py

--- bot/report_generator.py
+++ bot/report_generator.py
@@ -1,1 +1,12 @@
+import pandas as pd
+import datetime
 
+def generate_trade_report(trades):
+    # יצירת טבלה מדאטה
+    df = pd.DataFrame(trades)
+
+    # הוספת עמודת תאריך הפקה
+    df['report_date'] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    # שמירת הקובץ
+    df.to_excel('trade_management_log.xlsx', index=False)
